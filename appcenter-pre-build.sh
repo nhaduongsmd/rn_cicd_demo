45fd2a943257546d1ff42bff42883c10a6f8edb2
Nha Duong <nha.duong@smartdev.com>
2022-06-17 14:54:48 +0700

    Update

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Report build status next to bitbucket commit.
+#
+# Adjust settings in bitbucket.sh file
+#
+# Contributed by: DrBeak1
+# https://bitbucket.org/DrBeak1/
+# Copyright 2019 Zume, Inc.
+# SPDX Identifier: MIT
+
+echo "Pre Build"
